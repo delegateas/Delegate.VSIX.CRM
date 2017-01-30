506b373cc06a69ee96175a3c29240ded6b0bce6d
Martin Kasban Tange <mkt@delegate.dk>
2017-01-30 15:36:44 +0100

    Initial commit

---
+++ CrmPlugin/CrmPlugin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DG.XrmFramework.BusinessLogic;
+using DG.XrmFramework.BusinessLogic.Managers;
+using DG.XrmFramework.BusinessDomain.ServiceContext;
+
+namespace $rootnamespace$
+{
+    public class $itemname$ : Plugin
+    {
+
+        // Register when/how to execute
+        public $itemname$() : base(typeof($itemname$))
+        {
+            
+            RegisterPluginStep<Account>(
+                EventOperation.Create, 
+                ExecutionStage.PostOperation, 
+                Execute);
+            
+        }
+
+        // Execute plugin logic
+        protected void Execute(LocalPluginContext context) {
+        
+            // Insert logic here
+        
+        }
+    }
+}
---
+++ CrmWorkflowCodeActivity/CrmWorkflowCodeActivity.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+using DG.XrmFramework.BusinessDomain.ServiceContext;
+
+namespace $rootnamespace$ {
+
+    public sealed partial class $itemname$ : CodeActivity {
+
+        protected override void Execute(CodeActivityContext executionContext) {
+            var traceService = executionContext.GetExtension<ITracingService>()
+                as ITracingService;
+            var workflowExecutionContext =
+                executionContext.GetExtension<IWorkflowContext>()
+                as IWorkflowContext;
+            var factory =
+                executionContext.GetExtension<IOrganizationServiceFactory>()
+                as IOrganizationServiceFactory;
+            var orgService =
+                factory.CreateOrganizationService(workflowExecutionContext.UserId)
+                as IOrganizationService;
+            var orgAdminService =
+                factory.CreateOrganizationService(null)
+                as IOrganizationService;
+
+
+            var accountRef = this.AccountReference.Get(executionContext);
+
+            this.SomeStringOutput.Set(executionContext, "It works!");
+
+        }
+
+        // Define Input/Output Arguments
+        [RequiredArgument]
+        [Input("Some input entity reference")]
+        [ReferenceTarget(Account.EntityLogicalName)]
+        public InArgument<EntityReference> AccountReference { get; set; }
+
+        [Output("Some output")]
+        public OutArgument<string> SomeStringOutput { get; set; }
+
+    }
+}
